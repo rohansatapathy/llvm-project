ab01f18bcb4de39657eea44a6fdd51c25f70b65a
Rohan Satapathy <61920401+rohansatapathy@users.noreply.github.com>
2026-05-25 18:08:42 -0500

    [LC2K] Finish implementing LC2KTargetMachine + LC2KSubtarget
    Everything compiles so far, still need GISel to check end-to-end
    functional correctness.

--- llvm/lib/Target/LC2K/LC2KTargetMachine.h
+++ llvm/lib/Target/LC2K/LC2KTargetMachine.h
@@ -13,12 +13,14 @@
 #ifndef LLVM_LIB_TARGET_LC2K_LC2KTARGETMACHINE_H
 #define LLVM_LIB_TARGET_LC2K_LC2KTARGETMACHINE_H
 
+#include "LC2KSubtarget.h"
 #include "llvm/CodeGen/CodeGenTargetMachineImpl.h"
 #include <optional>
 
 namespace llvm {
 class LC2KTargetMachine : public CodeGenTargetMachineImpl {
   std::unique_ptr<TargetLoweringObjectFile> TLOF;
+  LC2KSubtarget Subtarget;
 
 public:
   LC2KTargetMachine(const Target &T, const Triple &TT, StringRef CPU,
@@ -26,6 +28,11 @@ public:
                     std::optional<Reloc::Model> RM,
                     std::optional<CodeModel::Model> CM, CodeGenOptLevel OL,
                     bool JIT);
+
+  const LC2KSubtarget *getSubtargetImpl() const { return &Subtarget; }
+  const LC2KSubtarget *getSubtargetImpl(const Function &) const override {
+    return &Subtarget;
+  }
 
   TargetPassConfig *createPassConfig(PassManagerBase &PM) override;
 
